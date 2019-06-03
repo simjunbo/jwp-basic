d8a23474f118c8e85bf0223254664be92d7e8f1b
simjunbo <s860620@gmail.com>
2019-06-03 22:49:01 +0900

    step8

--- src/main/java/core/mvc/DispatcherServlet.java
+++ src/main/java/core/mvc/DispatcherServlet.java
@@ -1,6 +1,7 @@
 package core.mvc;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,36 +9,68 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.common.collect.Lists;
+import core.nmvc.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
 public class DispatcherServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-    private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
+	private static final long serialVersionUID = 1L;
+	private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
+	private List<HandlerMapping> mappings = Lists.newArrayList();
+	private List<HandlerAdapter> handlerAdapters = Lists.newArrayList();
 
-    private RequestMapping rm;
+	@Override
+	public void init() throws ServletException {
+		LegacyHandlerMapping lhm = new LegacyHandlerMapping();
+		lhm.initMapping();
+		AnnotationHandlerMapping ahm = new AnnotationHandlerMapping("next.controller");
+		ahm.initialize();
 
-    @Override
-    public void init() throws ServletException {
-        rm = new RequestMapping();
-        rm.initMapping();
-    }
+		mappings.add(lhm);
+		mappings.add(ahm);
 
-    @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String requestUri = req.getRequestURI();
-        logger.debug("Method : {}, Request URI : {}", req.getMethod(), requestUri);
+		handlerAdapters.add(new ControllerHandlerAdapter());
+		handlerAdapters.add(new HandlerExecutionHandlerAdapter());
+	}
 
-        Controller controller = rm.findController(req.getRequestURI());
-        ModelAndView mav;
-        try {
-            mav = controller.execute(req, resp);
-            View view = mav.getView();
-            view.render(mav.getModel(), req, resp);
-        } catch (Throwable e) {
-            logger.error("Exception : {}", e);
-            throw new ServletException(e.getMessage());
-        }
-    }
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Object handler = getHandler(req);
+		if (handler == null) {
+			throw new IllegalArgumentException("존재하지 않는 URL입니다.");
+		}
+
+		try {
+			ModelAndView mav = execute(handler, req, resp);
+			View view = mav.getView();
+			view.render(mav.getModel(), req, resp);
+		} catch (Throwable e) {
+			logger.error("Exception : {}", e);
+			throw new ServletException(e.getMessage());
+		}
+	}
+
+	private Object getHandler(HttpServletRequest req) {
+		for (HandlerMapping handlerMapping : mappings) {
+			Object handler = handlerMapping.getHandler(req);
+			if (handler != null) {
+				return handler;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Controller는 Legacy
+	 */
+	private ModelAndView execute(Object handler, HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		for (HandlerAdapter handlerAdapter : handlerAdapters) {
+			if (handlerAdapter.supports(handler)) {
+				return handlerAdapter.handle(req, resp, handler);
+			}
+		}
+		return null;
+	}
 }
--- src/main/java/core/nmvc/AnnotationHandlerMapping.java
+++ src/main/java/core/nmvc/AnnotationHandlerMapping.java
@@ -1,29 +1,58 @@
 package core.nmvc;
 
-import java.util.Map;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import core.annotation.RequestMapping;
+import core.annotation.RequestMethod;
+import org.reflections.ReflectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Set;
 
-import com.google.common.collect.Maps;
+public class AnnotationHandlerMapping implements HandlerMapping {
+	private static final Logger logger = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+	private Object[] basePackage;
 
-import core.annotation.RequestMethod;
+	private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
 
-public class AnnotationHandlerMapping {
-    private Object[] basePackage;
+	public AnnotationHandlerMapping(Object... basePackage) {
+		this.basePackage = basePackage;
+	}
 
-    private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
+	public void initialize() {
+		ControllerScanner controllerScanner = new ControllerScanner(basePackage);
+		Map<Class<?>, Object> controllers = controllerScanner.getControllers();
+		Set<Method> methods = getRequestMappingMeethods(controllers.keySet());
 
-    public AnnotationHandlerMapping(Object... basePackage) {
-        this.basePackage = basePackage;
-    }
+		for (Method method : methods) {
+			RequestMapping rm = method.getAnnotation(RequestMapping.class);
+			logger.debug("register handlerExecution : url is {}, method is {}", rm.value(), method);
+			handlerExecutions.put(createHandlerKey(rm), new HandlerExecution(controllers.get(method.getDeclaringClass()), method));
+		}
 
-    public void initialize() {
+	}
 
-    }
+	private HandlerKey createHandlerKey(RequestMapping rm) {
+		return new HandlerKey(rm.value(), rm.method());
+	}
 
-    public HandlerExecution getHandler(HttpServletRequest request) {
-        String requestUri = request.getRequestURI();
-        RequestMethod rm = RequestMethod.valueOf(request.getMethod().toUpperCase());
-        return handlerExecutions.get(new HandlerKey(requestUri, rm));
-    }
+	@SuppressWarnings("unchecked")
+	private Set<Method> getRequestMappingMeethods(Set<Class<?>> controllers) {
+		Set<Method> requestMappingMethods = Sets.newHashSet();
+		for (Class<?> clazz : controllers) {
+			requestMappingMethods.addAll(ReflectionUtils.getAllMethods(clazz, ReflectionUtils.withAnnotation(RequestMapping.class)));
+		}
+		return requestMappingMethods;
+	}
+
+	public HandlerExecution getHandler(HttpServletRequest request) {
+		String requestUri = request.getRequestURI();
+		RequestMethod rm = RequestMethod.valueOf(request.getMethod().toUpperCase());
+		logger.debug("requestUri : {}, requestMethod : {}", requestUri, rm);
+		return handlerExecutions.get(new HandlerKey(requestUri, rm));
+	}
 }
